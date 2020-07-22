f1ed3237354a340422e9d81134beb415d10430a5
shp25 <68422722+shp25@users.noreply.github.com>
2020-07-22 21:11:01 +0530

    code.py
    this file code for assigning elements from list,accessing elements from tuple and deleting different elements from dictionary.

---
+++ code1.py
@@ -0,0 +1,18 @@
+# assigning elements to different lists
+a=[1,2,4,6]
+b=['pyton','java']
+c=a.append(5)
+d=b.append('c++')
+prrint(a)
+print(b)
+
+#accessing elements from a tuple
+tup1=('phy','chem','math',98,74,33)
+tup2=(2,4,6,8,10)
+print('tup1[0]:',tup1[0])
+print('tup2[1:3]:',tup2[1:3])
+
+#deleting different dictionary elements
+mydict={1:'phy',2:'chem',3:'maths')
+del mydict[3]
+print(mydict)
